724fbf564446f5bd17452cecd68b72574a001350
Joe Mondragon <jose.c.mondragon@gmail.com>
2019-10-17 13:23:12 -0500

    Perlin Noise
    Added shuffle

--- Deck.java
+++ Deck.java
@@ -12,11 +12,17 @@ public class Deck{
         cards.add( new Card(i, Card.SUIT.DIAMONDS ) );
         cards.add( new Card(i, Card.SUIT.HEARTS ) );
       }
+
   }
   public void shuffle(){
       for( int i = 0; i < cards.size() ; i++){
         // -> your implementation
-
+          int rand0= (int)(cards.size()*Math.abs(PerlinNoise.noise(Math.random()*255,Math.random()*255,Math.random()*255)));
+          int rand1= (int)(cards.size()*Math.abs(PerlinNoise.noise(Math.random()*255,Math.random()*255,Math.random()*255)));
+          Card a = cards.get(rand0);
+          Card b = cards.get(rand1);
+          cards.set(rand0,b);
+          cards.set(rand1,a);
       }
   }
   @Override
